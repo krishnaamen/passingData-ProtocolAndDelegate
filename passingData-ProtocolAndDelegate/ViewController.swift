197f35b81261249956d0a5b7fc14494ee6ad6167
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-06-26 08:11:00 +0200

    Initial Commit

---
+++ passingData-ProtocolAndDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  passingData-ProtocolAndDelegate
+//
+//  Created by Macbook on 26/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
