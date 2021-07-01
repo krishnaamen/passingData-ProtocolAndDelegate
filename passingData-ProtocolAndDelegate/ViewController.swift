c2b48534388a716622472bf13d454c6dd8dda500
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-07-01 08:23:54 +0200

    Second commit

--- passingData-ProtocolAndDelegate/ViewController.swift
+++ passingData-ProtocolAndDelegate/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,PassMyData {
+    
+    
 
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblAddress: UILabel!
+    @IBOutlet weak var lblCity: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func btnGoClicked(_ sender: UIButton) {
+        let SecondVC = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        SecondVC.delegate = self
+        self.navigationController?.pushViewController(SecondVC, animated: true)
+            }
+    func dataDetail(name: String, address: String, city: String) {
+        lblName.text = name
+        lblAddress.text = address
+        lblCity.text = city
+    }
+    
 }
+
 
